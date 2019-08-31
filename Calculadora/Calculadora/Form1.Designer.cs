f97e1154bcbe60c057597e99b2d5ff75e637a975
Diana Orbe <dorbe@ucol.mx>
2019-08-31 16:02:22 -0500

    Calculadora terminada

---
+++ Calculadora/Calculadora/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace Calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Num7 = new System.Windows.Forms.Button();
+            this.Num8 = new System.Windows.Forms.Button();
+            this.Num9 = new System.Windows.Forms.Button();
+            this.Num4 = new System.Windows.Forms.Button();
+            this.Num5 = new System.Windows.Forms.Button();
+            this.Num6 = new System.Windows.Forms.Button();
+            this.Num1 = new System.Windows.Forms.Button();
+            this.Num2 = new System.Windows.Forms.Button();
+            this.Num3 = new System.Windows.Forms.Button();
+            this.Punto = new System.Windows.Forms.Button();
+            this.BorrarT = new System.Windows.Forms.Button();
+            this.Dividir = new System.Windows.Forms.Button();
+            this.Mult = new System.Windows.Forms.Button();
+            this.Igual = new System.Windows.Forms.Button();
+            this.Porcen = new System.Windows.Forms.Button();
+            this.Sumar = new System.Windows.Forms.Button();
+            this.Restar = new System.Windows.Forms.Button();
+            this.Borrar1 = new System.Windows.Forms.Button();
+            this.Num0 = new System.Windows.Forms.Button();
+            this.Screen = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Num7
+            // 
+            this.Num7.Font = new System.Drawing.Font("Eras Medium ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Num7.Location = new System.Drawing.Point(26, 106);
+            this.Num7.Name = "Num7";
+            this.Num7.Size = new System.Drawing.Size(55, 46);
+            this.Num7.TabIndex = 0;
+            this.Num7.Text = "7";
+            this.Num7.UseVisualStyleBackColor = true;
+            this.Num7.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Num8
+            // 
+            this.Num8.Font = new System.Drawing.Font("Eras Medium ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Num8.Location = new System.Drawing.Point(109, 106);
+            this.Num8.Name = "Num8";
+            this.Num8.Size = new System.Drawing.Size(55, 46);
+            this.Num8.TabIndex = 1;
+            this.Num8.Text = "8";
+            this.Num8.UseVisualStyleBackColor = true;
+            this.Num8.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Num9
+            // 
+            this.Num9.Font = new System.Drawing.Font("Eras Medium ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Num9.Location = new System.Drawing.Point(187, 106);
+            this.Num9.Name = "Num9";
+            this.Num9.Size = new System.Drawing.Size(55, 46);
+            this.Num9.TabIndex = 2;
+            this.Num9.Text = "9";
+            this.Num9.UseVisualStyleBackColor = true;
+            this.Num9.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Num4
+            // 
+            this.Num4.Font = new System.Drawing.Font("Eras Medium ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Num4.Location = new System.Drawing.Point(26, 172);
+            this.Num4.Name = "Num4";
+            this.Num4.Size = new System.Drawing.Size(55, 46);
+            this.Num4.TabIndex = 3;
+            this.Num4.Text = "4";
+            this.Num4.UseVisualStyleBackColor = true;
+            this.Num4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // Num5
+            // 
+            this.Num5.Font = new System.Drawing.Font("Eras Medium ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Num5.Location = new System.Drawing.Point(109, 173);
+            this.Num5.Name = "Num5";
+            this.Num5.Size = new System.Drawing.Size(55, 46);
+            this.Num5.TabIndex = 4;
+            this.Num5.Text = "5";
+            this.Num5.UseVisualStyleBackColor = true;
+            this.Num5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // Num6
+            // 
+            this.Num6.Font = new System.Drawing.Font("Eras Medium ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Num6.Location = new System.Drawing.Point(187, 173);
+            this.Num6.Name = "Num6";
+            this.Num6.Size = new System.Drawing.Size(55, 46);
+            this.Num6.TabIndex = 5;
+            this.Num6.Text = "6";
+            this.Num6.UseVisualStyleBackColor = true;
+            this.Num6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // Num1
+            // 
+            this.Num1.Font = new System.Drawing.Font("Eras Medium ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Num1.Location = new System.Drawing.Point(26, 240);
+            this.Num1.Name = "Num1";
+            this.Num1.Size = new System.Drawing.Size(55, 46);
+            this.Num1.TabIndex = 6;
+            this.Num1.Text = "1";
+            this.Num1.UseVisualStyleBackColor = true;
+            this.Num1.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // Num2
+            // 
+            this.Num2.Font = new System.Drawing.Font("Eras Medium ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Num2.Location = new System.Drawing.Point(109, 240);
+            this.Num2.Name = "Num2";
+            this.Num2.Size = new System.Drawing.Size(55, 46);
+            this.Num2.TabIndex = 7;
+            this.Num2.Text = "2";
+            this.Num2.UseVisualStyleBackColor = true;
+            this.Num2.Click += new System.EventHandler(this.Button8_Click);
+            // 
+            // Num3
+            // 
+            this.Num3.Font = new System.Drawing.Font("Eras Medium ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Num3.Location = new System.Drawing.Point(187, 240);
+            this.Num3.Name = "Num3";
+            this.Num3.Size = new System.Drawing.Size(55, 46);
+            this.Num3.TabIndex = 8;
+            this.Num3.Text = "3";
+            this.Num3.UseVisualStyleBackColor = true;
+            this.Num3.Click += new System.EventHandler(this.Button9_Click);
+            // 
+            // Punto
+            // 
+            this.Punto.Font = new System.Drawing.Font("Eras Medium ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Punto.Location = new System.Drawing.Point(187, 304);
+            this.Punto.Name = "Punto";
+            this.Punto.Size = new System.Drawing.Size(55, 46);
+            this.Punto.TabIndex = 9;
+            this.Punto.Text = ".";
+            this.Punto.UseVisualStyleBackColor = true;
+            this.Punto.Click += new System.EventHandler(this.Button10_Click);
+            // 
+            // BorrarT
+            // 
+            this.BorrarT.Font = new System.Drawing.Font("Eras Medium ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BorrarT.Location = new System.Drawing.Point(289, 106);
+            this.BorrarT.Name = "BorrarT";
+            this.BorrarT.Size = new System.Drawing.Size(55, 46);
+            this.BorrarT.TabIndex = 10;
+            this.BorrarT.Text = "C";
+            this.BorrarT.UseVisualStyleBackColor = true;
+            this.BorrarT.Click += new System.EventHandler(this.Button11_Click);
+            // 
+            // Dividir
+            // 
+            this.Dividir.Font = new System.Drawing.Font("Eras Medium ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Dividir.Location = new System.Drawing.Point(289, 173);
+            this.Dividir.Name = "Dividir";
+            this.Dividir.Size = new System.Drawing.Size(55, 46);
+            this.Dividir.TabIndex = 11;
+            this.Dividir.Text = "/";
+            this.Dividir.UseVisualStyleBackColor = true;
+            this.Dividir.Click += new System.EventHandler(this.Button12_Click);
+            // 
+            // Mult
+            // 
+            this.Mult.Font = new System.Drawing.Font("Eras Medium ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Mult.Location = new System.Drawing.Point(289, 240);
+            this.Mult.Name = "Mult";
+            this.Mult.Size = new System.Drawing.Size(55, 46);
+            this.Mult.TabIndex = 12;
+            this.Mult.Text = "x";
+            this.Mult.UseVisualStyleBackColor = true;
+            this.Mult.Click += new System.EventHandler(this.Button13_Click);
+            // 
+            // Igual
+            // 
+            this.Igual.Font = new System.Drawing.Font("Eras Medium ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Igual.Location = new System.Drawing.Point(289, 304);
+            this.Igual.Name = "Igual";
+            this.Igual.Size = new System.Drawing.Size(55, 46);
+            this.Igual.TabIndex = 13;
+            this.Igual.Text = "=";
+            this.Igual.UseVisualStyleBackColor = true;
+            this.Igual.Click += new System.EventHandler(this.Button14_Click);
+            // 
+            // Porcen
+            // 
+            this.Porcen.Font = new System.Drawing.Font("Eras Medium ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Porcen.Location = new System.Drawing.Point(359, 303);
+            this.Porcen.Name = "Porcen";
+            this.Porcen.Size = new System.Drawing.Size(55, 46);
+            this.Porcen.TabIndex = 14;
+            this.Porcen.Text = "%";
+            this.Porcen.UseVisualStyleBackColor = true;
+            this.Porcen.Click += new System.EventHandler(this.Button15_Click);
+            // 
+            // Sumar
+            // 
+            this.Sumar.Font = new System.Drawing.Font("Eras Medium ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Sumar.Location = new System.Drawing.Point(359, 240);
+            this.Sumar.Name = "Sumar";
+            this.Sumar.Size = new System.Drawing.Size(55, 46);
+            this.Sumar.TabIndex = 15;
+            this.Sumar.Text = "+";
+            this.Sumar.UseVisualStyleBackColor = true;
+            this.Sumar.Click += new System.EventHandler(this.Button16_Click);
+            // 
+            // Restar
+            // 
+            this.Restar.Font = new System.Drawing.Font("Eras Medium ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Restar.Location = new System.Drawing.Point(359, 172);
+            this.Restar.Name = "Restar";
+            this.Restar.Size = new System.Drawing.Size(55, 46);
+            this.Restar.TabIndex = 16;
+            this.Restar.Text = "-";
+            this.Restar.UseVisualStyleBackColor = true;
+            this.Restar.Click += new System.EventHandler(this.Button17_Click);
+            // 
+            // Borrar1
+            // 
+            this.Borrar1.Font = new System.Drawing.Font("Eras Medium ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Borrar1.Location = new System.Drawing.Point(359, 107);
+            this.Borrar1.Name = "Borrar1";
+            this.Borrar1.Size = new System.Drawing.Size(55, 46);
+            this.Borrar1.TabIndex = 17;
+            this.Borrar1.Text = "CE";
+            this.Borrar1.UseVisualStyleBackColor = true;
+            this.Borrar1.Click += new System.EventHandler(this.Button18_Click);
+            // 
+            // Num0
+            // 
+            this.Num0.Font = new System.Drawing.Font("Eras Medium ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Num0.Location = new System.Drawing.Point(23, 304);
+            this.Num0.Name = "Num0";
+            this.Num0.Size = new System.Drawing.Size(141, 45);
+            this.Num0.TabIndex = 18;
+            this.Num0.Text = "0";
+            this.Num0.UseVisualStyleBackColor = true;
+            this.Num0.Click += new System.EventHandler(this.Button19_Click);
+            // 
+            // Screen
+            // 
+            this.Screen.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Screen.Location = new System.Drawing.Point(23, 32);
+            this.Screen.Name = "Screen";
+            this.Screen.Size = new System.Drawing.Size(377, 47);
+            this.Screen.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 372);
+            this.Controls.Add(this.Screen);
+            this.Controls.Add(this.Num0);
+            this.Controls.Add(this.Borrar1);
+            this.Controls.Add(this.Restar);
+            this.Controls.Add(this.Sumar);
+            this.Controls.Add(this.Porcen);
+            this.Controls.Add(this.Igual);
+            this.Controls.Add(this.Mult);
+            this.Controls.Add(this.Dividir);
+            this.Controls.Add(this.BorrarT);
+            this.Controls.Add(this.Punto);
+            this.Controls.Add(this.Num3);
+            this.Controls.Add(this.Num2);
+            this.Controls.Add(this.Num1);
+            this.Controls.Add(this.Num6);
+            this.Controls.Add(this.Num5);
+            this.Controls.Add(this.Num4);
+            this.Controls.Add(this.Num9);
+            this.Controls.Add(this.Num8);
+            this.Controls.Add(this.Num7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Num7;
+        private System.Windows.Forms.Button Num8;
+        private System.Windows.Forms.Button Num9;
+        private System.Windows.Forms.Button Num4;
+        private System.Windows.Forms.Button Num5;
+        private System.Windows.Forms.Button Num6;
+        private System.Windows.Forms.Button Num1;
+        private System.Windows.Forms.Button Num2;
+        private System.Windows.Forms.Button Num3;
+        private System.Windows.Forms.Button Punto;
+        private System.Windows.Forms.Button BorrarT;
+        private System.Windows.Forms.Button Dividir;
+        private System.Windows.Forms.Button Mult;
+        private System.Windows.Forms.Button Igual;
+        private System.Windows.Forms.Button Porcen;
+        private System.Windows.Forms.Button Sumar;
+        private System.Windows.Forms.Button Restar;
+        private System.Windows.Forms.Button Borrar1;
+        private System.Windows.Forms.Button Num0;
+        private System.Windows.Forms.TextBox Screen;
+    }
+}
+
---
+++ Calculadora/Calculadora/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        double primero;
+        double segundo;
+        double resultado;
+        string operación;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Screen.Text = Screen.Text + "7";
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Screen.Text = Screen.Text + "8";
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            Screen.Text = Screen.Text + "9";
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            Screen.Text = Screen.Text + "4";
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            Screen.Text = Screen.Text + "5";
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            Screen.Text = Screen.Text + "6";
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            Screen.Text = Screen.Text + "1";
+        }
+
+        private void Button8_Click(object sender, EventArgs e)
+        {
+            Screen.Text = Screen.Text + "2";
+        }
+
+        private void Button9_Click(object sender, EventArgs e)
+        {
+            Screen.Text = Screen.Text + "3";
+        }
+
+        private void Button19_Click(object sender, EventArgs e)
+        {
+            Screen.Text = Screen.Text + "0";
+        }
+
+        private void Button10_Click(object sender, EventArgs e)
+        {
+            Screen.Text = Screen.Text + ".";
+        }
+
+        private void Button11_Click(object sender, EventArgs e)
+        {
+            Screen.Clear();
+        }
+
+        private void Button18_Click(object sender, EventArgs e)
+        {
+            if (Screen.Text.Length == 1)
+            {
+                Screen.Text = "";
+            }
+            else
+            {
+                Screen.Text = Screen.Text.Substring(0, Screen.Text.Length - 1);
+            }
+        }
+
+        private void Button12_Click(object sender, EventArgs e)
+        {
+            operación = "/";
+            primero = double.Parse(Screen.Text);
+            Screen.Clear();
+        }
+
+        private void Button17_Click(object sender, EventArgs e)
+        {
+            operación = "-";
+            primero = double.Parse(Screen.Text);
+            Screen.Clear();
+
+        }
+
+        private void Button13_Click(object sender, EventArgs e)
+        {
+            operación = "*";
+            primero = double.Parse(Screen.Text);
+            Screen.Clear();
+
+        }
+
+        private void Button16_Click(object sender, EventArgs e)
+        {
+            operación = "+";
+            primero = double.Parse(Screen.Text);
+            Screen.Clear();
+
+        }
+
+        private void Button14_Click(object sender, EventArgs e)
+        {
+            segundo = double.Parse(Screen.Text);
+            switch (operación)
+            {
+                case "+":
+                    resultado = primero + segundo;
+                    Screen.Text = resultado.ToString();
+                    break;
+
+                case "-":
+                    resultado = primero - segundo;
+                    Screen.Text = resultado.ToString();
+                    break;
+
+                case "/":
+                    resultado = primero / segundo;
+                    Screen.Text = resultado.ToString();
+                    break;
+
+                case "*":
+                    resultado = primero * segundo;
+                    Screen.Text = resultado.ToString();
+                    break;
+
+                case "%":
+                    resultado = primero / 100 * segundo;
+                    Screen.Text = resultado.ToString();
+                    break;
+
+            }
+        }
+
+        private void Button15_Click(object sender, EventArgs e)
+        {
+            operación = "%";
+            primero = double.Parse(Screen.Text);
+            Screen.Clear();
+
+        }
+    }
+}
